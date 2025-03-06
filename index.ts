b06d54774336b775ae5cf9790c6430b14faa6e26
Ian Schwartz <ischwartz@cargurus.com>
2025-03-06 14:01:22 -0500

    HOTFIX: proper exporting

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ export * from "./src/Bool/Bool";
 export * from "./src/State/State";
 export * from "./src/Either/Either";
 export * from "./src/Result/Result";
+export * from "./src/Validation/Validation";
