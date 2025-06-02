003fccea0baca9209513c29fe30145c81632cae0
Ian Schwartz <ischwartz@cargurus.com>
2025-06-02 09:34:11 -0400

    trying something

---
+++ src/Effect/Effect.ts
@@ -0,0 +1,37 @@
+import { Result } from "../Result/Result";
+import { Maybe } from "../Maybe/Maybe";
+
+class Effect<T> {
+  private fn: () => Promise<T> | T;
+  private result: Maybe<Result<T>>;
+
+  constructor({
+    fn,
+    result = Maybe.nothing(),
+  }: {
+    fn: () => Promise<T> | T;
+    result?: Maybe<Result<T>>;
+  }) {
+    this.fn = fn;
+    this.result = result;
+  }
+
+  static of<T>(fn: () => Promise<T> | T) {
+    return new Effect<T>({ fn });
+  }
+
+  async run(): Promise<Effect<T>> {
+    const value = await this.fn();
+    return new Effect<T>({
+      fn: () => value,
+      result: Maybe.of(await Result.of(() => value as T)),
+    });
+  }
+}
+
+const example = async () => {
+  const E = Effect.of(() => "hello world");
+  return E.run();
+};
+
+example();
